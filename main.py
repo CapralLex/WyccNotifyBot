9284f14197712c00b7100b9ab5a908c6cc7ee431
CapralLex <lexxxoff@yandex.ru>
2020-06-18 20:37:53 +0300

    Change in the work of youtube checker

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 from threading import Thread
+from time import time, ctime, sleep
 
+import file_proc
 import longpoll_thread
 import steam_thread
 import tg_thread
 import twitch_thread
 import yt_thread
 
+
+def start():
+    try:
+
+        Thread(target=steam_thread.start_steam).start()
+        print('Steam thread running')
+
+        Thread(target=longpoll_thread.start_longpoll).start()
+        print('Longpoll thread running')
+
+        Thread(target=twitch_thread.start_twitch).start()
+        print('Twitch thread running')
+
+        Thread(target=yt_thread.start_yt).start()
+        print('YouTube thread running')
+
+        tg_thread.start_tg()
+
+    except Exception as exception:
+        file_proc.error_log(str(exception) + '| MAIN_STARTER')
+        print(exception, ctime(time()), 'MAIN_STARTER')
+        sleep(120)
+        start()
+
+
 if __name__ == "__main__":
-    Thread(target=steam_thread.start_steam).start()
-    print('Steam thread running')
-
-    Thread(target=longpoll_thread.start_longpoll).start()
-    print('Longpoll thread running')
-
-    Thread(target=twitch_thread.start_twitch).start()
-    print('Twitch thread running')
-
-    Thread(target=yt_thread.start_yt).start()
-    print('YouTube thread running')
-
-    tg_thread.start_tg()
+    start()
--- yt_thread.py
+++ yt_thread.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-from time import sleep
+from time import sleep, time, ctime
 
 import requests as r
 
@@ -11,54 +11,70 @@ def req(url):
     try:
         response = r.get(url).text
         return response
-    except r.exceptions.RequestException as error:
-        file_proc.error_log(error)
-        print(error)
+    except r.exceptions.RequestException as exception:
+        file_proc.error_log(str(exception) + '| YT(req)')
+        print(exception, ctime(time()), '| YT(req)')
         return None
 
 
 def processing(data):
-    xml = ET.fromstring(data)
-    last_entry = xml.find('{http://www.w3.org/2005/Atom}entry')
-    video_id = last_entry.find('{http://www.youtube.com/xml/schemas/2015}videoId').text
-    title = last_entry.find('{http://www.w3.org/2005/Atom}title').text
-    link = last_entry.find('{http://www.w3.org/2005/Atom}link').attrib['href']
-    return video_id, title, link
+    result = list()
+
+    try:
+        xml = ET.fromstring(data)
+    except Exception as exception:
+        file_proc.error_log(str(exception) + '| YT(xml)')
+        print(exception, ctime(time()), '| YT(req)')
+        return None
+
+    for entry in xml.findall('{http://www.w3.org/2005/Atom}entry'):
+        video_id = entry.find('{http://www.youtube.com/xml/schemas/2015}videoId').text
+        title = entry.find('{http://www.w3.org/2005/Atom}title').text
+        link = entry.find('{http://www.w3.org/2005/Atom}link_TEST').attrib['href']
+        result.append([video_id, title, link])
+    return result
 
 
 def to_send(title, link, index):
     channel = 'основном' if index == 0 else 'втором'
     message = f'Новое видео на {channel} канале:\n\n"{title}"\n{link}'
     vk_.send(message=message, category='youtube')
-    print(message)
+    print(f'New youtube video send successfully! {link}')
 
 
 def start_yt():
-    # n = 1
-    last_videos_ids = [[None], [None]]
     channels = file_proc.read_config('youtube', 'channels', list_=True)
+    last_videos_ids = dict()
+
+    # Первоначальное заполнение video_id
+    for index, channel in enumerate(channels):
+        # Сколько каналов в конфиге, столько и ключей словаря. Затем к ключу с id канала добавим id каждого видео
+        last_videos_ids.update({channel: list()})
+        response = req(url=f'https://www.youtube.com/feeds/videos.xml?channel_id={channel}')
+        items = processing(response)
+        for item in items:
+            last_videos_ids[channel].append(item[0])
+
+        last_videos_ids[channel].reverse()  # Разворачиваем список, чтобы последние видео были в конце
 
     while True:
-        for index in range(len(channels)):
-            response = req(url=f'https://www.youtube.com/feeds/videos.xml?channel_id={channels[index]}')
-            # print(f'Запрос {n} выполнен ({channels[index]})')
+        for channel_index, channel in enumerate(channels):
+            response = req(url=f'https://www.youtube.com/feeds/videos.xml?channel_id={channel}')
 
             if response is None:
                 sleep(15)
                 continue
 
-            # TODO: Получить все в случае, если новое видео. Иначе только id. Некритично
-            video_id, title, link = processing(response)
+            items = processing(response)
 
-            # Это нужно, чтобы при запуске бота он не отправлял последнее видео
-            if last_videos_ids[index][0] is None:
-                last_videos_ids[index][0] = video_id
+            if items is None:
+                sleep(15)
                 continue
 
-            if video_id not in last_videos_ids[index]:
-                to_send(title, link, index)
-                last_videos_ids[index].append(video_id)
+            for item in items:
+                if item[0] not in last_videos_ids[channel]:
+                    to_send(title=item[1], link=item[2], index=channel_index)
+                    last_videos_ids[channel].pop(0)
+                    last_videos_ids[channel].append(item[0])
 
-        # n += 1
-        # print('\n')
-        sleep(2)
+            sleep(2)
